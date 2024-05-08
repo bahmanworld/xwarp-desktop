cf1ff1bdc4b8044c408f8866ca270d2932d20a57
Bahman World <bahman.world@gmail.com>
2024-05-08 13:11:28 +0330

    some changes

--- src/panels/AboutPanel.tsx
+++ src/panels/AboutPanel.tsx
@@ -1,6 +1,7 @@
-import { ChevronLeft, Eraser } from "lucide-react";
+import { ChevronLeft, Eraser, ExternalLink } from "lucide-react";
 import { Button, ButtonGroup } from "@blueprintjs/core";
 import { usePanelStack } from "../stores/useStack";
+import React from "react";
 
 function AboutPanel() {
   const stack = usePanelStack();
@@ -36,14 +37,12 @@ function AboutPanel() {
         </Button>
         <div>
           <h3 style={{ margin: 0 }}>About XWarp</h3>
-          <div style={{ fontSize: 10, opacity: 0.4 }}>
-            Credits & Contributers
-          </div>
+          <div style={{ fontSize: 10, opacity: 0.4 }}>Devs & Contributers</div>
         </div>
         <div style={{ flex: 1 }} />
         <ButtonGroup>
           <Button
-          intent="success"
+            intent="success"
             onClick={() => {
               stack.pop();
             }}
@@ -57,29 +56,27 @@ function AboutPanel() {
 
       <div
         style={{
-          marginTop: 10,
-          fontFamily: "monospace",
-          wordBreak: "break-all",
-          lineBreak: "loose",
-          wordWrap: "break-word",
-          flex: 1,
-          height: "100%",
+          width: '100%',
+          height: '100%',
           display: "flex",
-          padding: 10,
           flexDirection: "column",
-          gap: 10,
-          overflowY: "auto",
         }}
       >
         <div
           style={{
+            width: '100%',
             flex: 1,
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
+            borderBottom: "1px dashed #fff1",
           }}
         >
-            Dev #1
+          <UserGithubAccount
+            photoUrl="https://avatars.githubusercontent.com/u/5371898?v=4"
+            username="bahmanworld"
+            githubLink="https://github.com/bahmanworld"
+          />
         </div>
         <div
           style={{
@@ -89,11 +86,52 @@ function AboutPanel() {
             alignItems: "center",
           }}
         >
-            Dev #2
+          <UserGithubAccount
+            photoUrl="https://avatars.githubusercontent.com/u/12181172?v=4"
+            username="ebdulrehmandemya"
+            githubLink="https://github.com/ebdulrehmandemya"
+          />
         </div>
       </div>
     </div>
   );
 }
+
+type UserGithubAccountProps = {
+  username: string;
+  photoUrl: string;
+  githubLink: string;
+};
+const UserGithubAccount: React.FC<UserGithubAccountProps> = (props) => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 3,
+      }}
+    >
+      <img
+        style={{ width: 120, height: 120, borderRadius: 120, marginBottom: 10 }}
+        src={props.photoUrl}
+      />
+      <div style={{ fontSize: 16, fontWeight: "bold" }}>{props.username}</div>
+      <a
+        href="#"
+        style={{ fontSize: 10, display: "flex", alignItems: "center", gap: 5 }}
+        onClick={(e) => {
+          e.preventDefault();
+          window.electron.openExternalLink(props.githubLink);
+        }}
+      >
+        <div>Open Github Profile</div>
+        <ExternalLink size={12} />
+      </a>
+    </div>
+  );
+};
 
 export default AboutPanel;
